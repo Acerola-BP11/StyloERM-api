5f87c8fe6c2cb04ee8804f36cc1e02d301cb2321
Acerola-BP11 <felipegrego23@outlook.com>
2024-03-05 16:05:13 -0300

    cors change

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var orderRouter = require('./routes/order')
 
 var app = express();
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://styloerm.vercel.app'
+}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
