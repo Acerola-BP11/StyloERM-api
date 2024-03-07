a194649f27434d625408a89960d61a28b06f766f
Acerola-BP11 <felipegrego23@outlook.com>
2024-03-07 10:42:45 -0300

    fix

--- utils/database.js
+++ utils/database.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(`${MONGO_URI}/?retryWrites=true&w=majority`);
+  await mongoose.connect(`${process.env.MONGO_URI}/?retryWrites=true&w=majority`);
 }
