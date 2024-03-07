aeb3060ebf37fc4b48fe06fae407ec5cf9f347a6
Acerola-BP11 <felipegrego23@outlook.com>
2024-03-07 10:34:21 -0300

    fix: security

--- utils/database.js
+++ utils/database.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://felipestruchel:sosoCACA1@styloerm.2p0xnzw.mongodb.net/?retryWrites=true&w=majority');
+  await mongoose.connect(`${MONGO_URI}/?retryWrites=true&w=majority`);
 }
