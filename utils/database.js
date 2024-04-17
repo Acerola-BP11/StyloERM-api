1ae397155e7a4d0373cd583d0a9b8d116a2fadd5
Acerola-BP11 <felipegrego23@outlook.com>
2024-04-17 14:08:46 -0300

    fix: security

--- utils/database.js
+++ utils/database.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://StyloAdmin:PaliZmS3T9WXT7OC@styloerm.2p0xnzw.mongodb.net/?retryWrites=true&w=majority&appName=StyloERM');
+  await mongoose.connect(process.env.MONGO_URI);
 } 
